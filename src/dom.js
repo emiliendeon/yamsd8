3dddce3cd3b05f80b99e2330a83627a0327ddea7
Émilien Déon <emilien.deon@gmail.com>
2025-09-25 23:07:07 +0200

    feat: dice rolling

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,13 @@
 const tbodyUpper = document.getElementById("upper");
-const tbodyLower = document.getElementById("lower");
+const trUpperSubTotal = tbodyUpper.querySelector(".sub-total");
 
-const trUpperSubTotal = document.querySelector("#upper tr.sub-total");
-const trLowerTotal = document.querySelector("#lower tr.section-total");
+const tbodyLower = document.getElementById("lower");
+const trLowerTotal = tbodyLower.querySelector(".section-total");
+
+const formTrack = document.getElementById("track");
+const divDice = document.getElementById("dice");
+const pRollsCount = document.getElementById("rolls-count");
+const formTrackSubmit = formTrack.querySelector("input[type='submit']");
 
 const combinationToTr = (combination) => {
 	const tr = document.createElement("tr");
@@ -18,4 +23,56 @@ const combinationToTr = (combination) => {
 	tr.appendChild(tdValue);
 
 	return tr;
+};
+
+const renderScoreboard = () => {
+	UpperCombinations.forEach((combination) => {
+		tbodyUpper.insertBefore(combinationToTr(combination), trUpperSubTotal);
+	});
+
+	LowerCombinations.forEach((combination) => {
+		tbodyLower.insertBefore(combinationToTr(combination), trLowerTotal);
+	});
+};
+
+const renderDie = (index, die) => {
+	const buttonDie = divDice.children[index];
+	buttonDie.removeAttribute("disabled");
+	buttonDie.ariaLabel = die.locked ? "Déverrouiller ce dé" : "Verrouiller ce dé";
+	buttonDie.classList.toggle("locked", die.locked);
+
+	const imgDie = buttonDie.firstElementChild;
+	imgDie.setAttribute("src", `assets/images/d8_${die.value}.png`);
+	imgDie.setAttribute("alt", die.value);
+};
+
+const renderDice = (diceState) => {
+	diceState.forEach((die, i) => renderDie(i, die));
+};
+
+const initDiceEvents = (onDieClick) => {
+	for (let i = 0; i < divDice.children.length; i++) {
+		divDice.children[i].addEventListener("click", () => onDieClick(i));
+	}
+};
+
+const renderCurrentRollsCount = (currentRollIndex, maxRollsCount) => {
+	pRollsCount.querySelector(".current").textContent = currentRollIndex ?? "-";
+
+	if (currentRollIndex >= maxRollsCount) {
+		for (let i = 0; i < divDice.children.length; i++) {
+			divDice.children[i].setAttribute("disabled", "");
+		}
+
+		formTrackSubmit.setAttribute("disabled", "");
+	}
+};
+
+const renderTrack = (maxRollsCount, onDieClick, onSubmit) => {
+	initDiceEvents(onDieClick);
+
+	renderCurrentRollsCount();
+	pRollsCount.querySelector(".total").textContent = maxRollsCount;
+
+	formTrack.addEventListener("submit", onSubmit);
 };
--- src/game.js
+++ src/game.js
@@ -1,11 +1,49 @@
-const initGame = () => {
-	UpperCombinations.forEach((combination) => {
-		tbodyUpper.insertBefore(combinationToTr(combination), trUpperSubTotal);
-	});
+const MAX_ROLLS_COUNT = 4;
 
-	LowerCombinations.forEach((combination) => {
-		tbodyLower.insertBefore(combinationToTr(combination), trLowerTotal);
-	});
+const gameState = {
+	currentRollIndex: 0,
+};
+
+let diceState = [];
+
+const resetDiceState = () => {
+	diceState = Array.from({ length: 5 }, () => ({ locked: false }));
+};
+
+const rollDice = () => {
+	for (let i = 0; i < diceState.length; i++) {
+		if (!diceState[i].locked) {
+			diceState[i].value = Math.floor(Math.random() * 8) + 1;
+		}
+	}
+
+	gameState.currentRollIndex++;
+};
+
+const onDieClick = (index) => {
+	if (gameState.currentRollIndex < MAX_ROLLS_COUNT) {
+		diceState[index].locked = !diceState[index].locked;
+
+		renderDie(index, diceState[index]);
+	}
+};
+
+const onTrackSubmit = (e) => {
+	e.preventDefault();
+
+	if (gameState.currentRollIndex < MAX_ROLLS_COUNT) {
+		rollDice();
+
+		renderDice(diceState);
+		renderCurrentRollsCount(gameState.currentRollIndex, MAX_ROLLS_COUNT);
+	}
+};
+
+const initGame = () => {
+	resetDiceState();
+
+	renderScoreboard();
+	renderTrack(MAX_ROLLS_COUNT, onDieClick, onTrackSubmit);
 };
 
 initGame();
